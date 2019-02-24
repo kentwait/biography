b4972bfa5d3e4d7b66ff2721cb40984cdacff33b
Kent Kawashima <kentkawashima@gmail.com>
2019-02-24 13:01:47 +0900

    Splits entry classes into a separate file

--- biography/biography.py
+++ biography/biography.py
@@ -5,8 +5,10 @@ import inspect
 import json
 import warnings
 
+from .entries import *
 
-__all__ = ['Biography']
+
+__all__ = ['Reporter']
 
 
 class PatchedModule:
@@ -25,7 +27,7 @@ class PatchedClass:
         else:
             func = self._builtin.__getattribute__(name)    
             if not name.startswith('_') and callable(func):
-                return self._reporter.watch_function(
+                return self._reporter.watch_method(
                     func, module=self._module)
             return func
 
@@ -33,7 +35,7 @@ class PatchedClass:
         res = PatchedClass(self._builtin(*args, **kwargs), self._reporter,
                       module=self._module)
         if sys._getframe().f_back.f_code.co_name in self._reporter.frames:
-            entry = Operation(self._builtin, args=args, kwargs=kwargs,
+            entry = Create(self._builtin, args=args, kwargs=kwargs,
                 module=self._module)
             self._reporter.entries.append(entry)
         self._reporter.tracked.append(self._builtin)
@@ -65,21 +67,21 @@ class Reporter:
     def forget(self, x):
         pass
 
-    def watch_function(self, func, module=None):
+    def watch_function(self, func, module=None, op=Operation):
         @wraps(func)
         def func_wrapper(*func_args, **func_kwargs):
             out = func(*func_args, **func_kwargs)
             # print(sys._getframe().f_back.f_code.co_name)
             if sys._getframe().f_back.f_code.co_name in self.frames:
-                entry = Operation(func, args=func_args, kwargs=func_kwargs,
+                entry = op(func, args=func_args, kwargs=func_kwargs,
                                   module=module)
                 self.entries.append(entry)
             self.tracked.append(func)
             return out
         return func_wrapper
 
-    def watch_method(self, func, module=None):
-        return self.watch_funct(func, module=module)
+    def watch_method(self, func, module=None, op=Method):
+        return self.watch_funct(func, module=module, op=op)
 
     def watch_class(self, cls=None, module=None):
         return self.watch_builtin(cls, module=module)
@@ -144,185 +146,3 @@ class Reporter:
 
     def __repr__(self):
         return repr(self.entries)
-
-
-class Operation:
-    def __init__(self, operation, args=None, kwargs=None,
-                 str_formatter=None, module=None):
-        self.operation = operation.__qualname__
-        if module:
-            self.operation = '.'.join([module, self.operation])
-        # IDEA: store obj instead of repr, could allow possibility of undo?
-        self.args = [repr(arg) for arg in args] if args else []
-        if operation.__name__ != operation.__qualname__ and \
-            '<locals>' not in operation.__qualname__:
-            self.args = self.args[1:]
-        self.kwargs = {key: repr(kwarg) for key, kwarg in kwargs.items()} \
-            if kwargs else dict()
-        self.datetime = datetime.now()
-        self._threshold_args = 3
-        self._threshold_kwargs = 2
-        self.str_formatter = str_formatter
-
-    def to_json(self, path=None):
-        return json.dumps(
-            {'Operation': 
-                {
-                    'datetime': self.datetime.strftime('%m/%d/%Y %H:%M:%S'),
-                    'operation': self.operation,
-                    'args': self.args,
-                    'kwargs': self.kwargs,
-                }
-            }
-        )
-
-    def to_markdown(self, path=None):
-        params = ''
-        if len(self.args) > 0:
-            params += ', '.join(self.args)
-        if len(self.kwargs) > 0:
-            params += ', ' + ', '.join(self.kwargs)
-        return ('## {op} operation\n'
-                '  * date and time - {dt}\n'
-                '  * statement - `{op}({params})`\n'.format(
-                   dt=self.datetime.strftime('%m/%d/%Y %H:%M:%S'),
-                   op=self.operation,
-                   params=params,
-                ))
-
-    def to_string(self, str_formatter=None, strftime='%m/%d/%Y %H:%M:%S'):
-        if str_formatter:
-            return str_formatter(
-                operation=self.operation,
-                args=self.args,
-                kwargs=self.kwargs,
-                datetime=self.datetime.strftime(strftime),
-            )
-        if self.str_formatter:
-            return self.str_formatter(
-                operation=self.operation,
-                args=self.args,
-                kwargs=self.kwargs,
-                datetime=self.datetime,
-            )
-        args = [v for i, v in enumerate(self.args)
-                if i < self._threshold_args]
-        if len(self.args) > self._threshold_args:
-            args += ['...']
-        kwargs = ['{k}={v}'.format(k=kv[0], v=kv[1]) 
-                for i, kv in enumerate(self.kwargs.items())
-                if i < self._threshold_args]
-        if len(self.kwargs) > self._threshold_kwargs:
-            kwargs += ['...']
-        params = ''
-        if len(args) > 0:
-            params += ', '.join(args)
-        if len(kwargs) > 0:
-            params += ', ' + ', '.join(kwargs)
-        return 'Operation {dt} {op}({params})'.format(
-            dt=self.datetime.strftime('%m/%d/%Y %H:%M:%S'),
-            op=self.operation,
-            params=params,
-        )
-
-    def to_toml(self, path=None):
-        return ('[Operation]\n'
-                'datetime = {dt}\n'
-                'operation = \'{op}\'\n'
-                'args = \'{args}\'\n'
-                'kwargs = \'{kwargs}\'\n'.format(
-                   dt=self.datetime.strftime('%m/%d/%Y %H:%M:%S'),
-                   op=self.operation,
-                   args=self.args,
-                   kwargs=self.kwargs,
-               ))
-
-    def to_csv(self, path=None, sep='\t'):
-        return sep.join([
-            'Operation',
-            self.datetime.strftime('%m/%d/%Y %H:%M:%S'),
-            self.operation,
-            str(self.args),
-            str(self.kwargs),
-        ])
-
-    def __str__(self):
-        return self.to_string()
-
-    def __repr__(self):
-        return '<{clsname} datetime={dt} statement={op}({params})>'.format(
-            clsname=self.__class__.__name__,
-            dt=self.datetime.strftime('%m/%d/%YT%H:%M:%S'),
-            op=self.operation,
-            params='...' if len(self.args) > 0 or len(self.kwargs) > 0 else ''
-        )
-
-class Comment:
-    def __init__(self, comment, str_formatter=None):
-        self.comment = comment
-        self.datetime = datetime.now()
-        self.str_formatter = str_formatter
-
-    def to_json(self, path=None):
-        return json.dumps(
-            {'Comment': 
-                {
-                    'datetime': self.datetime.strftime('%m/%d/%Y %H:%M:%S'),
-                    'comment': self.comment,
-                }
-            }
-        )
-
-    def to_markdown(self, path=None):
-        return ('## Comment\n'
-                '  * date and time - {dt}\n'
-                '  * comment - {comment}\n'.format(
-                   dt=self.datetime.strftime('%m/%d/%Y %H:%M:%S'),
-                   comment=self.comment,
-                ))
-
-    def to_string(self, str_formatter, strftime='%m/%d/%Y %H:%M:%S'):
-        if str_formatter:
-            return str_formatter(
-                comment=self.comment,
-                datetime=self.datetime.strftime(strftime),
-            )
-        if self.str_formatter:
-            return self.str_formatter(
-                comment=self.comment,
-                datetime=self.datetime.strftime(strftime),
-            )
-        return 'Comment {dt} {comment}'.format(
-            dt=self.datetime.strftime('%m/%d/%Y %H:%M:%S'),
-            comment=self.comment,
-        )
-
-    def to_toml(self, path=None):
-        return ('[Comment]\n'
-                'datetime = {dt}\n'
-                'comment = {comment}\n'.format(
-                   dt=self.datetime.strftime('%m/%d/%Y %H:%M:%S'),
-                   comment=self.comment,
-                ))
-
-    def to_csv(self, path=None, sep='\t'):
-        return sep.join([
-            'Comment',
-            self.datetime.strftime('%m/%d/%Y %H:%M:%S'),
-            self.comment,
-        ])
-
-    def __str__(self):
-        if self.str_formatter:
-            return self.str_formatter(self.comment, self.datetime)
-        return 'Comment {dt} "{msg}"'.format(
-            dt=self.datetime.strftime('%m/%d/%Y %H:%M:%S'),
-            msg=self.comment,
-        )
-
-    def __repr__(self):
-        return '<{clsname} datetime={dt} comment="{msg}">'.format(
-            clsname=self.__class__.__name__,
-            dt=self.datetime.strftime('%m/%d/%YT%H:%M:%S'),
-            msg=self.comment,
-        )
---
+++ biography/entries.py
@@ -0,0 +1,214 @@
+import json
+
+
+__all__ = [
+    'Operation', 'Create', 'Method', 
+    'Comment'
+]
+
+
+class Operation:
+    def __init__(self, operation, args=None, kwargs=None,
+                 str_formatter=None, module=None):
+        self.operation = operation.__qualname__
+        self.optype = 'operation'
+        if module:
+            self.operation = '.'.join([module, self.operation])
+        # IDEA: store obj instead of repr, could allow possibility of undo?
+        self.args = [repr(arg).replace('\n', ' ') for arg in args] \
+            if args else []
+        if operation.__name__ != operation.__qualname__ and \
+            '<locals>' not in operation.__qualname__:
+            self.args = self.args[1:]
+        self.kwargs = {key: repr(kwarg).replace('\n', ' ') 
+                       for key, kwarg in kwargs.items()} \
+            if kwargs else dict()
+        self.datetime = datetime.now()
+        self._threshold_args = 3
+        self._threshold_kwargs = 2
+        self.str_formatter = str_formatter
+
+    def to_json(self, path=None):
+        return json.dumps(
+            {self.optype: 
+                {
+                    'datetime': self.datetime.strftime('%m/%d/%Y %H:%M:%S'),
+                    'operation': self.operation,
+                    'args': self.args,
+                    'kwargs': self.kwargs,
+                }
+            }
+        )
+
+    def to_markdown(self, path=None):
+        params = ''
+        if len(self.args) > 0:
+            params += ', '.join(self.args)
+        if len(self.kwargs) > 0:
+            params += ', ' + ', '.join(self.kwargs)
+        return ('## {optype} {op}\n'
+                '  * date and time - {dt}\n'
+                '  * statement - `{op}({params})`\n'.format(
+                   dt=self.datetime.strftime('%m/%d/%Y %H:%M:%S'),
+                   op=self.operation,
+                   optype=self.optype,
+                   params=params,
+                ))
+
+    def to_string(self, str_formatter=None, strftime='%m/%d/%Y %H:%M:%S'):
+        if str_formatter:
+            return str_formatter(
+                operation=self.operation,
+                optype=self.optype,
+                args=self.args,
+                kwargs=self.kwargs,
+                datetime=self.datetime.strftime(strftime),
+            )
+        if self.str_formatter:
+            return self.str_formatter(
+                operation=self.operation,
+                optype=self.optype,
+                args=self.args,
+                kwargs=self.kwargs,
+                datetime=self.datetime,
+            )
+        args = [v for i, v in enumerate(self.args)
+                if i < self._threshold_args]
+        if len(self.args) > self._threshold_args:
+            args += ['...']
+        kwargs = ['{k}={v}'.format(k=kv[0], v=kv[1]) 
+                for i, kv in enumerate(self.kwargs.items())
+                if i < self._threshold_args]
+        if len(self.kwargs) > self._threshold_kwargs:
+            kwargs += ['...']
+        params = ''
+        if len(args) > 0:
+            params += ', '.join(args)
+        if len(kwargs) > 0:
+            params += ', ' + ', '.join(kwargs)
+        return '{optype} {dt} {op}({params})'.format(
+            dt=self.datetime.strftime('%m/%d/%Y %H:%M:%S'),
+            optype=self.optype,
+            op=self.operation,
+            params=params,
+        )
+
+    def to_toml(self, path=None):
+        return ('[{optype}]\n'
+                'datetime = {dt}\n'
+                'operation = \'{op}\'\n'
+                'args = \'{args}\'\n'
+                'kwargs = \'{kwargs}\'\n'.format(
+                   dt=self.datetime.strftime('%m/%d/%Y %H:%M:%S'),
+                   op=self.operation,
+                   optype=self.optype,
+                   args=self.args,
+                   kwargs=self.kwargs,
+               ))
+
+    def to_csv(self, path=None, sep='\t'):
+        return sep.join([
+            self.optype,
+            self.datetime.strftime('%m/%d/%Y %H:%M:%S'),
+            self.operation,
+            str(self.args),
+            str(self.kwargs),
+        ])
+
+    def __str__(self):
+        return self.to_string()
+
+    def __repr__(self):
+        return '<{clsname} datetime={dt} statement={op}({params})>'.format(
+            clsname=self.__class__.__name__,
+            dt=self.datetime.strftime('%m/%d/%YT%H:%M:%S'),
+            op=self.operation,
+            params='...' if len(self.args) > 0 or len(self.kwargs) > 0 else ''
+        )
+
+
+class Create(Operation):
+    def __init__(self, operation, args=None, kwargs=None,
+                 str_formatter=None, module=None):
+        super().__init__(operation, args=args, kwargs=kwargs,
+                         str_formatter=str_formatter, module=module)
+        self.optype = 'create'
+
+
+class Method(Operation):
+    def __init__(self, operation, args=None, kwargs=None,
+                 str_formatter=None, module=None):
+        super().__init__(operation, args=args, kwargs=kwargs,
+                         str_formatter=str_formatter, module=module)
+        self.optype = 'method'
+
+
+class Comment:
+    def __init__(self, comment, str_formatter=None):
+        self.comment = comment
+        self.datetime = datetime.now()
+        self.str_formatter = str_formatter
+
+    def to_json(self, path=None):
+        return json.dumps(
+            {'Comment': 
+                {
+                    'datetime': self.datetime.strftime('%m/%d/%Y %H:%M:%S'),
+                    'comment': self.comment,
+                }
+            }
+        )
+
+    def to_markdown(self, path=None):
+        return ('## Comment\n'
+                '  * date and time - {dt}\n'
+                '  * comment - {comment}\n'.format(
+                   dt=self.datetime.strftime('%m/%d/%Y %H:%M:%S'),
+                   comment=self.comment,
+                ))
+
+    def to_string(self, str_formatter, strftime='%m/%d/%Y %H:%M:%S'):
+        if str_formatter:
+            return str_formatter(
+                comment=self.comment,
+                datetime=self.datetime.strftime(strftime),
+            )
+        if self.str_formatter:
+            return self.str_formatter(
+                comment=self.comment,
+                datetime=self.datetime.strftime(strftime),
+            )
+        return 'Comment {dt} {comment}'.format(
+            dt=self.datetime.strftime('%m/%d/%Y %H:%M:%S'),
+            comment=self.comment,
+        )
+
+    def to_toml(self, path=None):
+        return ('[Comment]\n'
+                'datetime = {dt}\n'
+                'comment = {comment}\n'.format(
+                   dt=self.datetime.strftime('%m/%d/%Y %H:%M:%S'),
+                   comment=self.comment,
+                ))
+
+    def to_csv(self, path=None, sep='\t'):
+        return sep.join([
+            'Comment',
+            self.datetime.strftime('%m/%d/%Y %H:%M:%S'),
+            self.comment,
+        ])
+
+    def __str__(self):
+        if self.str_formatter:
+            return self.str_formatter(self.comment, self.datetime)
+        return 'Comment {dt} "{msg}"'.format(
+            dt=self.datetime.strftime('%m/%d/%Y %H:%M:%S'),
+            msg=self.comment,
+        )
+
+    def __repr__(self):
+        return '<{clsname} datetime={dt} comment="{msg}">'.format(
+            clsname=self.__class__.__name__,
+            dt=self.datetime.strftime('%m/%d/%YT%H:%M:%S'),
+            msg=self.comment,
+        )
